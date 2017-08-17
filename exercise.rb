783ac0996b42bae61dcee064ae0b51e9c8c18d01
Marlon O'Neil <hello@marlononeil.com>
2017-08-16 23:11:31 -0400

    completed exercise 2

--- exercise.rb
+++ exercise.rb
@@ -16,5 +16,24 @@ def sum_of_nums
 end
 
 #Array of numbers
-list_of_numbers = *(1..20)
-puts "The sum of all odd numbers '#{list_of_numbers}' is #{new_method(list_of_numbers)}"
+# list_of_numbers = *(1..20)
+# puts "The sum of all odd numbers '#{list_of_numbers}' is #{new_method(list_of_numbers)}"
+
+
+#Exercise 2
+
+# Pick three names and store them in an array.
+names_array = ["marlon", "sherel", "peter"]
+
+# Prompt the user to enter their name.
+print "Please enter your name: "
+new_name = gets.strip
+
+# If their name is one of the names in the array, display a greeting message that includes their name.
+
+  if names_array.include?(new_name.downcase)
+    puts "Hello #{new_name}! How are you today?"
+  else
+    # their name isn't in the list, display "Who goes there?"
+    puts "Who goes there?"
+  end
